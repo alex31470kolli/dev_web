5bd85ad98c0ae20dd69c0130b992828517f6905b
MatheoCatto <matheo.catto@etu.cyu.fr>
2026-04-25 10:47:01 +0200

    Update connexion.js

--- connexion.js
+++ connexion.js
@@ -1,18 +1,26 @@
 const formulaireConnexion = document.getElementById("formulaire-connexion");
 const messageErreur = document.getElementById("message-erreur");
 
-formulaireConnexion.addEventListener("submit", function (evenement) {
+formulaireConnexion.addEventListener("submit", async function (evenement) {
   evenement.preventDefault();
 
-  const courriel = document.getElementById("courriel").value;
-  const motDePasse = document.getElementById("mot-de-passe").value;
-  
-/* J'ai mis ce qu'il y a en dessous pour tester mais faudra l'enlever quand vous aurez fait php*/
+  const formData = new FormData(formulaireConnexion);
 
-  if (courriel === "etudiant@test.com" && motDePasse === "1234") {
-    alert("Connexion réussie !");
-    window.location.href = "pageetudiant.html";
-  } else {
-    messageErreur.textContent = "Email ou mot de passe incorrect.";
+  try {
+    const response = await fetch('traitement_connexion.php', {
+      method: 'POST',
+      body: formData
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+        // Redirection vers l'accueil étudiant (en PHP désormais)
+        window.location.href = "pageetudiant.php"; 
+    } else {
+        messageErreur.textContent = result.message;
+    }
+  } catch (error) {
+    messageErreur.textContent = "Erreur de connexion au serveur.";
   }
 });
